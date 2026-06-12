c030b51f2445e1331c1d1dc6e862748d23bcc651
gurunathan.m@rapiddatatech.com <gurunathan.m@rapiddatatech.com>
2026-06-12 12:51:26 +0530

    fix: final push to achieve green CI status

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        jvmToolchain(17)
-    }
     buildFeatures {
         viewBinding = true
     }
